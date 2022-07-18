c54bd73b7f7c7f315716cc6e4cb8482a0454fec2
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-19 00:15:07 +0600

    [DONE/ADD] initial setup and work now am gonna drive into deep

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,28 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+import connectDB from "./database/mdb.js";
 
+dotenv.config({ path: "./backend/configs/config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cookieParser());
+
+connectDB();
+
+app.all("/", (req, res) => {
+  console.log(`Welcome to the ${process.env.APP_NAME} Console 🚀 Viewer`);
+  return res.status(200).send({
+    message: `Welcome to the ${process.env.APP_NAME} App 👨🏻‍💻 Viewer`,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
