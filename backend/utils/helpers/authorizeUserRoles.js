4b78b3cb075233980dc65ee5439eb89e44a2c431
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-23 17:35:11 +0600

    [ERROR] trying to solving

--- backend/utils/helpers/authorizeUserRoles.js
+++ backend/utils/helpers/authorizeUserRoles.js
@@ -12,7 +12,7 @@ export const authorizeUserRoles = (roles = []) => {
   return [
     async (req, res, next) => {
       try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.headers.token;
         if (token) {
           const decoded = jwt.verify(token, process.env.JWT_SECRET);
           req.user = decoded;
