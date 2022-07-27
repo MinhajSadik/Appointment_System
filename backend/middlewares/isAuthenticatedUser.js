cacdeb9bb5e6a8cd971e89cd3b7398cb87cc7678
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-27 18:03:17 +0600

    [TOKEN] releted working registration request time could not get the token

--- backend/middlewares/isAuthenticatedUser.js
+++ backend/middlewares/isAuthenticatedUser.js
@@ -3,27 +3,16 @@ import UserModel from "../models/userModel.js";
 
 export const isAuthenticatedUser = async (req, res, next) => {
   try {
-    //check if the token is string or not
-    if (typeof req.headers.authorization === "string") {
-      const token = req.headers.authorization.split(" ")[1];
-      const isCustomAuth = token.length < 500;
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
 
-      if (token && isCustomAuth) {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await UserModel.findById(decoded.id);
-        req.user = user;
-      }
-      return next();
-    } else {
-      //if token is not string
-      const token = req.headers.token;
-      if (token) {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await UserModel.findById(decoded.id);
-        req.user = user;
-        return next();
-      }
+    if (token && isCustomAuth) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await UserModel.findById(decoded.id);
+      req.user = user;
     }
+
+    return next();
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({
@@ -31,37 +20,3 @@ export const isAuthenticatedUser = async (req, res, next) => {
     });
   }
 };
-
-// export const isAuthenticatedUser2 = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-//     const isCustomAuth = token.length < 500;
-//     // const token = req.cookies.token;
-
-//     if (token && isCustomAuth) {
-//       // if (token) {
-//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//       const user = await UserModel.findById(decoded.id);
-//       req.user = user;
-//     }
-//     return next();
-//   } catch (error) {
-//     console.error(error.message);
-//     return res.status(500).json({
-//       message: `Server Error: ${error.message}`,
-//     });
-//   }
-// };
-
-// export const authorizeRoles = (...roles) => {
-//   return (req, res, next) => {
-//     if (req.user) {
-//       if (roles.length && !roles.includes(req.user.role)) {
-//         return res.status(401).json({
-//           message: `${req.user.role} you are not authorized to perform this action`,
-//         });
-//       }
-//     }
-//     return next();
-//   };
-// };
