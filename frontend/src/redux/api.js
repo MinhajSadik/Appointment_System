293c7672ed9e64dfa8132be5ae1199503eff21d4
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-28 19:23:02 +0600

    [DONE] everything is gonna okay now it is time to deploy

--- frontend/src/redux/api.js
+++ frontend/src/redux/api.js
@@ -4,13 +4,13 @@ const devEnv = process.env.NODE_ENV === "development";
 
 const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
-// const API = axios.create({
-//   baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
-// });
-
 const API = axios.create({
-  baseURL: process.env.REACT_APP_PROD_API,
+  baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
 });
+
+// const API = axios.create({
+//   baseURL: process.env.REACT_APP_PROD_API,
+// });
 
 console.log(REACT_APP_DEV_API, REACT_APP_PROD_API, devEnv);
 
