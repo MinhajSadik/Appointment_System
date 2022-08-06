ebe46a3d19ba596d3dda146b23505a0986f739db
MinhajSadik <MinhajoSadik@gmail.com>
2022-08-07 02:04:54 +0600

    [UPDATED] UI Changed and appointment will be updated live and change will data without refresh

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -155,29 +155,3 @@ const Appointments = () => {
 };
 
 export default Appointments;
-
-<div class="container mx-auto px-4 sm:px-8 max-w-3xl">
-  <div class="py-8">
-    <div class="flex flex-row mb-1 sm:mb-0 justify-between w-full">
-      <h2 class="text-2xl leading-tight">Users</h2>
-      <div class="text-end">
-        <form class="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
-          <div class=" relative ">
-            <input
-              type="text"
-              id='"form-subscribe-Filter'
-              class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-              placeholder="name"
-            />
-          </div>
-          <button
-            class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
-            type="submit"
-          >
-            Filter
-          </button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>;
